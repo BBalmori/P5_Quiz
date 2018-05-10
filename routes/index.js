f26f6814fc2a923dbb0c60a9c59ea57ebde9897f
Belen Balmori <36365130+BBalmori@users.noreply.github.com>
2018-05-10 12:28:16 +0200

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 
-const { models } = require('../models/index.js');
+const sequelize = require('../models/index.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
