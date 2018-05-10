62f89c0c3e66f66c34a9804a681674695cc2b13b
Belen Balmori <36365130+BBalmori@users.noreply.github.com>
2018-05-10 11:26:14 +0200

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+//const Sequelize = require('sequelize');
+const { models } = require('../models');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'QUIZ' });
+});
+
+////////
+router.get('/credits', function (req, res, next) {
+    res.render('index', { title: 'Belen Balmori' });
+});
+
+router.get('/quizzes', function (req, res, next) {
+    sequelize.models.quiz.findAll()
+        .then(quiz => {
+            res.render('quizzes', { array: quiz });
+        })
+        .catch(error => next(error));
+});
+////////
+
+module.exports = router;
